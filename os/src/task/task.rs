5991fd1b4a590f8065330e4a9c833a55f8efde73
ZhengQuancheng <zhengqc@yeah.net>
2025-10-04 16:54:46 +0800

    Implement `sys_trace`

--- os/src/task/task.rs
+++ os/src/task/task.rs
@@ -1,5 +1,5 @@
 //! Types related to task management
-
+use crate::config::MAX_SYSCALL_NUM;
 use super::TaskContext;
 
 /// The task control block (TCB) of a task.
@@ -9,6 +9,8 @@ pub struct TaskControlBlock {
     pub task_status: TaskStatus,
     /// The task context
     pub task_cx: TaskContext,
+    /// The syscall counts
+    pub syscall_counts: [usize; MAX_SYSCALL_NUM],
 }
 
 /// The status of a task
