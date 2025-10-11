67eb820eb4ad88040afb7968bf21bd30b5408a20
ZhengQuancheng <zhengqc@yeah.net>
2025-10-11 09:37:37 +0800

    reimplement `sys_get_time`

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -1,5 +1,9 @@
 //! Process management syscalls
-use crate::task::{change_program_brk, exit_current_and_run_next, suspend_current_and_run_next};
+use crate::{
+    mm::translated_byte_buffer,
+    timer::get_time_us,
+    task::{change_program_brk, current_user_token, exit_current_and_run_next, suspend_current_and_run_next}
+};
 
 #[repr(C)]
 #[derive(Debug)]
@@ -27,7 +31,33 @@ pub fn sys_yield() -> isize {
 /// HINT: What if [`TimeVal`] is splitted by two pages ?
 pub fn sys_get_time(_ts: *mut TimeVal, _tz: usize) -> isize {
     trace!("kernel: sys_get_time");
-    -1
+    // 获取当前时间
+    let us = get_time_us();
+    let tv = TimeVal {
+        sec: us / 1_000_000,
+        usec: us % 1_000_000,
+    };
+    // 将应用地址空间中的一段缓冲区 _ts 转化为在内核地址空间直接读写的字节切片向量 bufs
+    let bufs = translated_byte_buffer(
+        current_user_token(),
+        _ts as *const u8,
+        core::mem::size_of::<TimeVal>()
+    );
+    // 将 tv 转为字节数组, 方便数据复制
+    let src = unsafe {
+        core::slice::from_raw_parts(
+            &tv as *const TimeVal as *const u8,
+            core::mem::size_of::<TimeVal>(),
+        )
+    };
+    // 将 src 拷贝分批到 bufs 中
+    let mut offset = 0;
+    for buf in bufs {
+        let len = core::cmp::min(buf.len(), src.len() - offset);
+        buf[..len].copy_from_slice(&src[offset..offset + len]);
+        offset += len;
+    }
+    0
 }
 
 /// TODO: Finish sys_trace to pass testcases
